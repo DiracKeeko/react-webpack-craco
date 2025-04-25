fd2826dc33e73d4fba95374d5d839d0ef98d13ff
Keeko <787018661@qq.com>
2025-04-25 16:29:07 +0800

    [feat] craco-html逻辑变更

--- src/cracoPlugin/craco-html.js
+++ src/cracoPlugin/craco-html.js
@@ -1,22 +1,49 @@
-const { getPlugin, pluginByName, whenProd } = require('@craco/craco');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { getPlugin, pluginByName, whenProd } = require("@craco/craco");
 
 module.exports = {
   overrideWebpackConfig: ({ webpackConfig, pluginOptions }) => {
-    const { isFound, match } = getPlugin(webpackConfig, pluginByName('HtmlWebpackPlugin'));
-    if (isFound) {
-      const { userOptions } = match;
-      const { publicPath = '', filename = 'index.html' } = pluginOptions;
+    const { isFound, match } = getPlugin(
+      webpackConfig,
+      pluginByName("HtmlWebpackPlugin")
+    );
 
-      userOptions.publicPath = publicPath;
-      userOptions.inject = 'body';
-      userOptions.scriptLoading = 'blocking';
+    if (isFound) {
+      const oldPlugin = match;
+      const { options: oldOptions } = oldPlugin;
+      const {
+        publicPath = "",
+        filename = "index.html",
+        inject = "head",
+        scriptLoading = "defer",
+      } = pluginOptions;
+
+      let newOptions = {
+        ...oldOptions,
+        publicPath,
+        inject: "body",
+        scriptLoading: "blocking",
+      };
 
       whenProd(() => {
-        userOptions.meta = {
-          ...userOptions.meta,
+        newOptions.meta = {
+          ...newOptions.meta,
           timestamp: `${Date.now()}`,
         };
-        userOptions.filename = filename;
+        newOptions = {
+          ...newOptions,
+          filename,
+          inject,
+          scriptLoading
+        };
+      });
+
+      // 替换 HtmlWebpackPlugin 实例
+      webpackConfig.plugins = webpackConfig.plugins.map((plugin) => {
+        if (plugin.constructor.name === "HtmlWebpackPlugin") {
+          return new HtmlWebpackPlugin(newOptions);
+        }
+        return plugin;
       });
     }
 
