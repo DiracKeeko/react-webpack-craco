886e09d2976ea1e136f9f7406d330f310a0a7fbc
Keeko <787018661@qq.com>
2025-05-13 10:23:00 +0800

    [feat] 两个根据配置生成导航+路由的App文件

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,6 @@
 // craco.config.js
 const path = require("path");
-const { CracoHtmlPlugin } = require("./cracoPlugin/craco-html");
+// const { CracoHtmlPlugin } = require("./cracoPlugin/craco-html");
 
 module.exports = {
   webpack: {
@@ -8,13 +8,13 @@ module.exports = {
       "@": path.join(__dirname, "src"), // 允许通过@符号来表示 src目录
     },
   },
-  plugins: [
-    {
-      plugin: CracoHtmlPlugin,
-      options: {
-        inject: "body",
-        scriptLoading: "blocking",
-      },
-    },
-  ],
+  // plugins: [
+  //   {
+  //     plugin: CracoHtmlPlugin,
+  //     options: {
+  //       inject: "body",
+  //       scriptLoading: "blocking",
+  //     },
+  //   },
+  // ],
 };
---
+++ src/AppPlus01.tsx
@@ -0,0 +1,51 @@
+// src/App.tsx
+// 入口文件 仅有一层路由，不需要Outlet
+
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
+const routes = [
+  { path: "/home", component: "Home" },
+  { path: "/about", component: "About" },
+];
+
+// 主应用组件
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <div id="app">
+        {/* 导航栏 */}
+        <div id="nav">
+          01
+          {routes.map((route) => {
+            return (
+              <>
+                <Link to={route.path}>{route.component}</Link> | 
+              </>
+            );
+          })}
+        </div>
+
+        {/* 路由视图 */}
+        <Suspense>
+          <Routes>
+            {routes.map((route) => {
+              const Component = lazy(
+                () => import(`@/pages/${route.component}`)
+              );
+              return (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  element={<Component />}
+                />
+              );
+            })}
+          </Routes>
+        </Suspense>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
